ea422976b734b85fd515372e2b93d7784478ecd5
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-07-20 19:22:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import WeatherDisplay from '../WeatherDisplay'
 function App() {
 const [weather, setWeather] = useState({
   temperature:25,
-  conditions: "Sunny",
+  Conditions: "Sunny",
 });
 
 useEffect(() => { 
