6bd93ebb62f2bcea948e1df8e8efd3ccc62d9328
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-07-20 18:54:34 +0530

    Update WeatherDisplay.jsx

--- src/WeatherDisplay.jsx
+++ src/WeatherDisplay.jsx
@@ -1,1 +1,25 @@
 
+import react from 'react'
+
+function WeatherDisplay({weather}) {
+const tempColour = weather.temprature > 20 ? "red" : "blue";
+
+
+return (
+
+    <div>
+    <p>
+      Temprature:{" "}
+      <span style={{color: tempColour}}>
+        {weather.temprature}
+        </span>  
+    </p>
+    <p>
+      condition: {weather.condition}  
+    </p>
+    </div>
+);
+
+
+}
+export default WeatherDisplay;
